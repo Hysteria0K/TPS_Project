7f51ed21630af8ea5c23f4a1e4cba511f702a88c
Hysteria0K <tksthqo123@gmail.com>
2024-03-07 10:30:24 +0900

    카메라 완

--- TPS_Project/Assets/Script/Camera.cs
+++ TPS_Project/Assets/Script/Camera.cs
@@ -10,25 +10,26 @@ public class Camera : MonoBehaviour
     public Vector3 Pl_Pos;
 
     public Vector3 Camera_Center;
+
     public float radius = 2.685f;
-    public float speed = 2f;
+    public float speed = 1.5f;
 
     public float angle = 2.81f;
-    public float acos2;
+    public float acos;
+
+    public float rotation_X;
     // Start is called before the first frame update
     void Start()
     {
-        /*Pl_Pos = Player.GetComponent<Player>().Player_Position;
 
-        Pl_Pos.y = Pl_Pos.y + 2.05f;
-        Pl_Pos.x = Pl_Pos.x + 0.87f;
-        Pl_Pos.z = Pl_Pos.z - 2.54f;
-
-        this.transform.position = Pl_Pos;*/
     }
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+    void FixedUpdate()
     {
         Pl_Pos = Player.GetComponent<Player>().Player_Position;
 
@@ -36,21 +37,20 @@ public class Camera : MonoBehaviour
 
         Camera_Center = Pl_Pos;
 
-        angle += speed * Time.deltaTime;
+        rotation_X = Input.GetAxis("Mouse X") * speed;
+
+        angle += rotation_X * Time.deltaTime; // 앵글 = 파이값, 마우스 움직이는거에 넣으면 될듯
+
+        if (angle < 0) angle += 2*Mathf.PI;
+        else if (angle > 2*Mathf.PI) angle -= 2*Mathf.PI;
 
         this.transform.position = Camera_Center + new Vector3(Mathf.Sin(angle), 0, Mathf.Cos(angle)) * radius;
 
-        acos2 = Mathf.Acos(Mathf.Cos(angle))*180.0f/Mathf.PI;
+        acos = Mathf.Acos(Mathf.Cos(angle))*180.0f/Mathf.PI;
 
-        if (Mathf.FloorToInt(angle / Mathf.PI )% 2 == 1)
-        {
-            acos2 = -acos2;
-            Debug.Log("ㅇ");
-        }
+        if (Mathf.FloorToInt(angle / Mathf.PI) % 2 == 1) acos = -acos;
 
-        Quaternion rotation = Quaternion.Euler(new Vector3(0, acos2 - 161.0828f, 0));
-        this.transform.rotation = rotation;
-       
+        this.transform.rotation = Quaternion.Euler(new Vector3(0, acos - 161.0828f, 0));
 
     }
 }
--- TPS_Project/Assets/Script/Player.cs
+++ TPS_Project/Assets/Script/Player.cs
@@ -2,33 +2,45 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float Player_Speed = 100.0f;
+    public float Player_Speed = 10.0f;
     public float rotationSpeed = 360f;
 
     public Vector3 direction;
     public Vector3 Player_Position;
 
     CharacterController characterController;
+
+    public GameObject Camera;
+
+    public Quaternion Cam_direction;
     // Start is called before the first frame update
     void Start()
     {
         characterController= GetComponent<CharacterController>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+      
+    }
+
+    void FixedUpdate()
+    {
+        Cam_direction = Camera.GetComponent<Transform>().rotation;
+
+        direction = Cam_direction * new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 
         if (direction.sqrMagnitude > 0.01f)
         {
             Vector3 forward = Vector3.Slerp(transform.forward, direction,
             rotationSpeed * Time.deltaTime / Vector3.Angle(transform.forward, direction));
+
             transform.LookAt(transform.position + forward);
         }
 
         characterController.Move(direction * Player_Speed * Time.deltaTime);
         Player_Position = this.transform.position;
-
     }
 }
