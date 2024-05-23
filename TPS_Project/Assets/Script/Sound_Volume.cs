779bfa0507d1bb87af1dcd4f9e484aa3270e876f
Hysteria0K <tksthqo123@gmail.com>
2024-05-23 12:25:58 +0900

    진짜 최종완

--- TPS_Project/Assets/Script/Sound_Volume.cs
+++ TPS_Project/Assets/Script/Sound_Volume.cs
@@ -1,21 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Sound_Volume : MonoBehaviour
 {
     private Setting_Saver Setting_Saver;
     private AudioSource audioSource;
+    private GameObject Sound_Box;
+
 
     private void Awake()
     {
         Setting_Saver = GameObject.Find("Setting_Saver").GetComponent<Setting_Saver>();
         audioSource = this.GetComponent<AudioSource>();
+        Sound_Box = GameObject.Find("Sound_Box");
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (SceneManager.GetActiveScene().name == "Stage_2_Clear")
+        {
+            Destroy(Sound_Box);
+        }
     }
 
     // Update is called once per frame
